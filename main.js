74cbb87684fa68c95d18aa2ce29d7324654ba6ce
Rachel <rachelshenhom@gmail.com>
2021-04-27 15:39:57 -0400

    strings to words

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ console.log('Hello Kargo');
 //create an array of integers
 const intergers = [8, 16, 19, 94];
 intergers;
+//convert numbers to String using map
+const stringOfNumbers = integers.map(String);
+stringOfNumbers;
+
+
+//convert string into words
+var convert = require('number-to-words');
+convert.toWords(stringOfNumbers);
