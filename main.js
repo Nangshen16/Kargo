2c240bcc19689fca7de7eb4aa843770c020cbaf3
Rachel <rachelshenhom@gmail.com>
2021-04-28 13:13:47 -0400

    commentout

--- main.js
+++ main.js
@@ -10,3 +10,47 @@ stringOfNumbers;
 //convert string into words
 var convert = require('number-to-words');
 convert.toWords(stringOfNumbers);
+// process.stdout.property(numbersOfString);
+  // const convert = numbersOfString.map((i)=>
+  // stringOfNumbers(i));
+  // console.log(convert);
+  // num = (numbers) => {
+  //   for(var i=0; i <numbers.length; i++){
+  //     console.log(numbers[i]);
+  //   }
+  // };
+  // num(numbers);
+  
+// //forEach function
+// const forEach = (array, callback) => {
+//   for(let i =0; i< array.length; i++){
+//     callback(array[i]);
+//   }
+// }
+// //invoke forEach
+// forEach(
+//   [stringOfNumbers],
+//   (currentArrayElement) => {
+//     console.log(currentArrayElement + words);
+//   }
+// );
+
+
+// let final = stringOfNumbers.map(words);
+
+// const newArray = Array.from(words);
+// console.log(newArray);
+// words.reverse().join('');
+// Let s = words.toString();
+// for (let i=0; i< words.length; i++){
+//   console.log(words[i]);
+// for (const i in words){
+//   console.log(`row ${i}`);{
+//     for (const j in words[i]){
+//       console.log(`${words[i]`);
+   
+//     }
+    
+//   }
+// }
+
